12d1216585a0cf7f264708e76e88639efcf19b23
Sharad <sharadg8@gmail.com>
2016-06-15 21:46:23 +0100

    Add card view - dummy

--- app/src/main/java/com/sharad/cards/CardsActivity.java
+++ app/src/main/java/com/sharad/cards/CardsActivity.java
@@ -1,7 +1,12 @@
 package com.sharad.cards;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 
 import com.sharad.epocket.R;
 
@@ -11,5 +16,41 @@ public class CardsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
+
+        HorizontalScrollView scrollView = (HorizontalScrollView)findViewById(R.id.horizontalScrollView);
+        EditText name = (EditText)findViewById(R.id.c_add_name);
+        EditText number = (EditText)findViewById(R.id.c_add_number);
+        EditText expiry = (EditText)findViewById(R.id.c_add_expiry);
+        EditText cvv = (EditText)findViewById(R.id.c_add_cvv);
+        EditText pin = (EditText)findViewById(R.id.c_add_pin);
+        EditText contact = (EditText)findViewById(R.id.c_add_contact);
+
+        setScrollTo(name, scrollView);
+        setScrollTo(number, scrollView);
+        setScrollTo(expiry, scrollView);
+        setScrollTo(cvv, scrollView);
+        setScrollTo(pin, scrollView);
+        setScrollTo(contact, scrollView);
+    }
+
+    private void setScrollTo(final EditText view, final HorizontalScrollView scrollView) {
+        view.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    final int count = ((LinearLayout) scrollView.getChildAt(0))
+                            .getChildCount();
+                    for (int i = 0; i < count; i++) {
+                        final View child = ((TextInputLayout)((LinearLayout) scrollView.getChildAt(0)).getChildAt(i)).getEditText();
+                        if (child != view) {
+                            scrollTo += child.getMeasuredWidth();
+                        } else {
+                            break;
+                        }
+                    }
+                    scrollView.smoothScrollTo(scrollTo, 0);
+                }
+            }
+        });
     }
 }
